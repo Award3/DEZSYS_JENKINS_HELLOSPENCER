26e84ce93d56c6162b9b379e400ed65a501f5fb0
Nico Gal <ngal@student.tgm.ac.at>
2026-05-12 14:16:57 +0200

    test lastlast

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,7 +8,7 @@ class AppTest {
     @Test
     void testGreet() {
         App app = new App();
-        assertEquals("Hello, hallo!", app.greet("Nico"));
+        assertEquals("Hello, Nico!", app.greet("Nico"));
     }
 
     @Test
